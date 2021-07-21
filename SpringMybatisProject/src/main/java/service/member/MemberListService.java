2a75c27cf75dd71d04bb4ba2e8ee29b06a468c6e
ezen <ezen@DESKTOP-O03MT84>
2021-07-21 12:24:41 +0900

    202107211224 이숭무

--- SpringMybatisProject/src/main/java/service/member/MemberListService.java
+++ SpringMybatisProject/src/main/java/service/member/MemberListService.java
@@ -14,27 +14,27 @@ public class MemberListService {
 	@Autowired
 	MemberRepository memberRepository;
 	public void memList(Model model,String memId, Integer page) {
+		MemberDTO dto = new MemberDTO();
 		int limit = 5;
 		int limitPage = 10;
 		//                      2  - 1
-		Long startRow = ((long)page -1 ) * limit +1;
-		Long endRow = startRow + limit -1;
-		StartEndPageDTO sep = new StartEndPageDTO();
-		sep.setStartRow(startRow);
-		sep.setEndRow(endRow);
-		
-		MemberDTO dto = new MemberDTO();
-		dto.setStartEndPageDTO(sep);
+		if(page != null) {
+			Long startRow = ((long)page -1 ) * limit +1;
+			Long endRow = startRow + limit -1;
+			StartEndPageDTO sep = new StartEndPageDTO();
+			sep.setStartRow(startRow);
+			sep.setEndRow(endRow);
+			dto.setStartEndPageDTO(sep);
+		}
 		dto.setMemId(memId);
-		
-		
 		List<MemberDTO> list = memberRepository.memList(dto);
 		Integer count = memberRepository.getMemberCount();
 		model.addAttribute("lists", list);
 		model.addAttribute("count", count);
-		
-		PageAction pageAction = new PageAction();
-		pageAction.page(count, limit, page, limitPage, model, 
-				"memList");
+		if(page != null) {
+			PageAction pageAction = new PageAction();
+			pageAction.page(count, limit, page, limitPage, model, 
+					"memList");
+		}
 	}
 }
